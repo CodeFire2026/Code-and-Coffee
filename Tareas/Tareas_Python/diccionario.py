4beb63132de7f53c759ed0c54efd7c87828be04d
ElBusta27 <brunonicolasbustamante@gmail.com>
2026-08-31 20:20:10 -0300

    Agrego tarea diccionario de jugadores clase 3 python

---
+++ Tareas/Tareas_Python/diccionario.py
@@ -0,0 +1,84 @@
+seleccionArgentina = {
+    23: {
+        "nombre": "Emiliano Martinez",
+        "edad": 33,
+        "altura": 1.95,
+        "precio": "20 millones",
+        "posicion": "arquero"
+    },
+
+    13: {
+        "nombre": "Cristian Romero",
+        "edad": 28,
+        "altura": 1.85,
+        "precio": "40 millones",
+        "posicion": "defensor central"
+    },
+
+    3: {
+        "nombre": "Nicolas Tagliafico",
+        "edad": 33,
+        "altura": 1.72,
+        "precio": "7 millones",
+        "posicion": "lateral izquierdo"
+    },
+
+    4: {
+        "nombre": "Gonzalo Montiel",
+        "edad": 29,
+        "altura": 1.78,
+        "precio": "10 millones",
+        "posicion": "lateral derecho"
+    },
+
+    5: {
+        "nombre": "Leandro Paredes",
+        "edad": 32,
+        "altura": 1.80,
+        "precio": "8 millones",
+        "posicion": "mediocampista"
+    },
+
+    7: {
+        "nombre": "Rodrigo De Paul",
+        "edad": 32,
+        "altura": 1.78,
+        "precio": "15 millones",
+        "posicion": "mediocampista"
+    },
+
+    20: {
+        "nombre": "Alexis Mac Allister",
+        "edad": 27,
+        "altura": 1.76,
+        "precio": "100 millones",
+        "posicion": "mediocampista"
+    },
+
+    8: {
+        "nombre": "Enzo Fernandez",
+        "edad": 25,
+        "altura": 1.80,
+        "precio": "110 millones",
+        "posicion": "mediocampista"
+    },
+
+    9: {
+        "nombre": "Julian Alvarez",
+        "edad": 26,
+        "altura": 1.70,
+        "precio": "100 millones",
+        "posicion": "delantero"
+    },
+
+    22: {
+        "nombre": "Lautaro Martinez",
+        "edad": 29,
+        "altura": 1.74,
+        "precio": "95 millones",
+        "posicion": "delantero"
+    }
+}
+
+for llave in seleccionArgentina:
+    print(llave, seleccionArgentina[llave])
